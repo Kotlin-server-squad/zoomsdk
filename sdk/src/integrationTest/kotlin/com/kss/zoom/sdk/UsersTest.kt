6ec978e59ec823d3e9382b5b92c894402bf9ed39
Jiří Hermann <jiri.hermann@gendigital.com>
2024-02-29 18:50:55 +0100

    CRUD users

--- sdk/src/integrationTest/kotlin/com/kss/zoom/sdk/UsersTest.kt
+++ sdk/src/integrationTest/kotlin/com/kss/zoom/sdk/UsersTest.kt
@@ -1,5 +1,10 @@
 package com.kss.zoom.sdk
 
+import com.kss.zoom.sdk.users.CreateUserRequest
+import com.kss.zoom.sdk.users.UserInfo
+import com.kss.zoom.sdk.users.Users
+import com.kss.zoom.utils.call
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -13,7 +18,50 @@ class UsersTest : ZoomTestBase() {
     }
 
     @Test
-    fun `should load`() {
+    fun `create user`() = runBlocking {
+        val request = CreateUserRequest(
+            action = CreateUserRequest.Action.create, userInfo = UserInfo(
+                email = "kotlinserversquad+test1@gmail.com",
+                firstName = "Kotlin",
+                lastName = "Server",
+                displayName = "KotlinServer",
+                type = 1
+            )
+        )
+
+        val user = call {
+            users.create(request)
+        }
+
+        println(user)
+
+    }
+
+    @Test
+    fun `get user`() = runBlocking {
+        val user = call {
+            users.get("dNeRClc2Q1Ce-tT5c1kHAQ")
+        }
+
+        println(user)
+    }
+
+    @Test
+    fun `get delete`() = runBlocking {
+        val user = call {
+            users.delete("dNeRClc2Q1Ce-tT5c1kHAQ")
+        }
+
+        println(user)
+
+    }
+
+    @Test
+    fun `get all user`() = runBlocking {
+        val users = call {
+            users.list()
+        }
+        users.forEach(::println)
 
     }
 }
--- sdk/src/main/kotlin/com/kss/zoom/sdk/model/users.kt
+++ sdk/src/main/kotlin/com/kss/zoom/sdk/model/users.kt
@@ -1,5 +1,8 @@
 package com.kss.zoom.sdk.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
 data class User(
     val id: UserId,
     val email: Email,
@@ -14,12 +17,12 @@ enum class UserType {
     None
 }
 
-data class CreateUser(
-    val email: Email,
-    val firstName: String,
-    val lastName: String,
-    val displayName: String? = null
-)
+//data class CreateUser(
+//    val email: Email,
+//    val firstName: String,
+//    val lastName: String,
+//    val displayName: String? = null
+//)
 
 data class UpdateUser(
     val email: Email? = null,
@@ -30,3 +33,56 @@ data class UpdateUser(
 
 typealias Email = String
 typealias UserId = String
+
+
+@Serializable
+data class CreateUserRequest(
+    val action: Action,
+    @SerialName("user_info")
+    val userInfo: UserInfo,
+){
+    enum class Action {
+        create,
+        ssoCreate,
+    }
+}
+
+@Serializable
+data class UserInfo(
+    val email: Email? = null,
+    @SerialName("first_name")
+    val firstName: String? = null,
+    @SerialName("last_name")
+    val lastName: String? = null,
+    @SerialName("display_name")
+    val displayName: String? = null,
+    val type: Int,
+)
+
+@Serializable
+data class CreateUserResponse(
+    @SerialName("first_name")
+    val firstName: String,
+    @SerialName("last_name")
+    val lastName: String,
+    val id: String,
+    val email: String,
+    val type: Int,
+)
+
+data class GetUsersResponse(
+    @SerialName("total_records")
+    val totalRecords: Int,
+    val users: List<GetUser>
+)
+
+@Serializable
+data class GetUser(
+    val id: String,
+    val email: String,
+    @SerialName("first_name")
+    val firstName: String,
+    @SerialName("last_name")
+    val lastName: String,
+    val type: Int,
+)
