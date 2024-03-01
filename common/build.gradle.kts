a9e41309c6319ae97cb4dafa2f1dcee8568de1bf
zezutom <zezulatomas@gmail.com>
2024-03-01 14:42:39 +0100

    Dependencies update.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,12 +4,14 @@ plugins {
 
 val kotlin_version: String by project
 val ktor_version: String by project
+val kotlinx_coroutines_slf4j_version: String by project
+val logback_version: String by project
 
 dependencies {
 
     // Core dependencies
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.8.0")
-    implementation("ch.qos.logback:logback-classic:1.2.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:$kotlinx_coroutines_slf4j_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
 
     // Ktor client
     implementation("io.ktor:ktor-client-core:$ktor_version")
