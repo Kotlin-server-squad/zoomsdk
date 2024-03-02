79d36c9d39f56447af43572df96374420c7adc28
zezutom <zezulatomas@gmail.com>
2024-03-02 13:32:31 +0100

    Meetings: Happy path unit tests

--- sdk/src/main/kotlin/com/kss/zoom/sdk/Meetings.kt
+++ sdk/src/main/kotlin/com/kss/zoom/sdk/Meetings.kt
@@ -1,17 +1,14 @@
 package com.kss.zoom.sdk
 
-import com.kss.zoom.PagedQuery
 import com.kss.zoom.Page
+import com.kss.zoom.PagedQuery
 import com.kss.zoom.auth.UserTokens
 import com.kss.zoom.client.WebClient
 import com.kss.zoom.sdk.model.*
 import com.kss.zoom.toIsoString
 import com.kss.zoom.toWebClient
 import io.ktor.client.*
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
-import java.time.ZonedDateTime
 import java.util.*
 
 interface Meetings : ZoomModule {
@@ -82,11 +79,11 @@ class MeetingsImpl private constructor(
         timezone: TimeZone
     ): Result<Meeting> {
         assert(duration > 0) { "Duration must be greater than 0" }
-        return client.post<Http.MeetingResponse>(
+        return client.post<MeetingResponse>(
             url = "$ZOOM_API_URL/users/$userId/meetings",
             token = userTokens!!.accessToken.value,
             contentType = WebClient.JSON_CONTENT_TYPE,
-            body = Http.CreateOneTimeMeetingRequest(
+            body = CreateOneTimeMeetingRequest(
                 topic = topic,
                 type = 2,
                 startTime = startTime.toIsoString(),
@@ -97,7 +94,7 @@ class MeetingsImpl private constructor(
     }
 
     override suspend fun get(meetingId: Long): Result<Meeting> =
-        client.get<Http.MeetingResponse>(
+        client.get<MeetingResponse>(
             url = "$ZOOM_API_URL/meetings/$meetingId",
             token = userTokens!!.accessToken.value
         ).map { it.toModel() }
@@ -131,115 +128,10 @@ class MeetingsImpl private constructor(
         query.nextPageToken?.let {
             params.append("&next_page_token=$it")
         }
-        return client.get<Http.PaginationObject>(
+        return client.get<PaginationObject>(
             url = "$ZOOM_API_URL/users/$userId/meetings?$params",
             token = userTokens!!.accessToken.value
         ).map { it.toModel() }
     }
 }
 
-private fun Http.PaginationObject.toModel(): Page<ScheduledMeeting> =
-    Page(
-        items = this.meetings.map { it.toModel() },
-        pageNumber = pageNumber,
-        pageCount = pageCount,
-        pageSize = pageSize,
-        totalRecords = totalRecords,
-        nextPageToken = nextPageToken
-    )
-
-private fun Http.MeetingResponse.toModel(): Meeting =
-    Meeting(
-        id = id,
-        uuid = uuid,
-        topic = topic,
-        host = MeetingHost(
-            id = hostId,
-            email = hostEmail
-        ),
-        startTime = ZonedDateTime.parse(startTime).toLocalDateTime(),
-        duration = duration,
-        joinUrl = joinUrl,
-        startUrl = startUrl,
-        password = password,
-        createdAt = ZonedDateTime.parse(createdAt).toInstant(),
-        timeZone = TimeZone.getTimeZone(timezone),
-    )
-
-private fun Http.ScheduledMeeting.toModel(): ScheduledMeeting =
-    ScheduledMeeting(
-        id = id,
-        uuid = uuid,
-        hostId = hostId,
-        agenda = agenda,
-        topic = topic,
-        startTime = ZonedDateTime.parse(startTime).toInstant(),
-        duration = duration,
-        joinUrl = joinUrl,
-        createdAt = ZonedDateTime.parse(createdAt).toInstant(),
-        timeZone = TimeZone.getTimeZone(timezone)
-    )
-
-private object Http {
-    @Serializable
-    data class CreateOneTimeMeetingRequest(
-        val topic: String,
-        val type: Short,
-        @SerialName("start_time") val startTime: String? = null,
-        val duration: Short? = null,
-        val timezone: String? = null
-    )
-
-    @Serializable
-    data class MeetingResponse(
-        val id: Long,
-        val uuid: String,
-        val topic: String,
-        val duration: Short,
-        @SerialName("host_id") val hostId: String,
-        @SerialName("host_email") val hostEmail: String,
-        val status: String,
-        @SerialName("start_time") val startTime: String,
-        val timezone: String,
-        @SerialName("created_at") val createdAt: String,
-        @SerialName("start_url") val startUrl: String,
-        @SerialName("join_url") val joinUrl: String,
-        val password: String
-    )
-
-    @Serializable
-    data class ScheduledMeeting(
-        val id: Long,
-        val uuid: String,
-        @SerialName("host_id") val hostId: String,
-        val agenda: String? = null,
-        val topic: String,
-        val duration: Short,
-        @SerialName("start_time") val startTime: String,
-        val timezone: String,
-        @SerialName("created_at") val createdAt: String,
-        @SerialName("join_url") val joinUrl: String
-    )
-
-    @Serializable
-    data class PaginationObject(
-        @SerialName("page_count") val pageCount: Int,
-        @SerialName("page_number") val pageNumber: Int,
-        @SerialName("page_size") val pageSize: Int,
-        @SerialName("total_records") val totalRecords: Int,
-        val meetings: List<ScheduledMeeting>,
-        @SerialName("next_page_token") val nextPageToken: String? = null
-    )
-
-    @Serializable
-    data class Recurrence(
-        val type: Short,
-        @SerialName("end_date_time") val endDateTime: String? = null,
-        @SerialName("end_times") val endTimes: Short? = null,
-        @SerialName("monthly_day") val monthlyDay: Short? = null,
-        @SerialName("monthly_week") val monthlyWeek: Short? = null,
-        @SerialName("monthly_week_day") val monthlyWeekDay: Short? = null,
-        @SerialName("repeat_interval") val repeatInterval: Short? = null,
-        @SerialName("weekly_days") val weeklyDays: List<Short>? = null
-    )
-}
--- sdk/src/test/kotlin/com/kss/zoom/sdk/MeetingsTest.kt
+++ sdk/src/test/kotlin/com/kss/zoom/sdk/MeetingsTest.kt
@@ -1,14 +1,14 @@
 package com.kss.zoom.sdk
 
-import com.kss.zoom.sdk.ZoomMock.CLIENT_TIMEZONE
-import com.kss.zoom.sdk.ZoomMock.CONSTANT_CLOCK
-import com.kss.zoom.sdk.ZoomMock.USER_ID
-import com.kss.zoom.sdk.ZoomMock.module
+import com.kss.zoom.sdk.model.MeetingResponse
+import com.kss.zoom.sdk.model.PaginationObject
+import com.kss.zoom.sdk.model.toModel
 import com.kss.zoom.utils.call
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
+import kotlin.test.assertEquals
 
 class MeetingsTest : ModuleTest<Meetings>() {
     override fun module(): Meetings = meetings()
@@ -16,31 +16,30 @@ class MeetingsTest : ModuleTest<Meetings>() {
         module.listScheduled(USER_ID)
 
     @Test
-    fun `should schedule meeting`() = runBlocking {
+    fun `should create meeting`() = runBlocking {
+        val jsonResponse = """
+            {
+              "uuid": "dzFrpLGqRcOtpiQQeAVDVA==",
+              "id": 78723497365,
+              "host_id": "lqkrEKqMR1CCmALIVs73RQ",
+              "host_email": "zezulatomas@gmail.com",
+              "topic": "Your Meeting Title",
+              "type": 2,
+              "status": "waiting",
+              "start_time": "2024-02-21T08:49:58Z",
+              "duration": 60,
+              "timezone": "America/Los_Angeles",
+              "created_at": "2024-02-21T08:49:58Z",
+              "start_url": "https://us04web.zoom.us/s/78723497365?zak=eyJ0eXAiOiJKV1QiLCJzdiI6IjAwMDAwMSIsInptX3NrbSI6InptX28ybSIsImFsZyI6IkhTMjU2In0.eyJhdWQiOiJjbGllbnRzbSIsInVpZCI6Imxxa3JFS3FNUjFDQ21BTElWczczUlEiLCJpc3MiOiJ3ZWIiLCJzayI6IjAiLCJzdHkiOjEsIndjZCI6InVzMDQiLCJjbHQiOjAsIm1udW0iOiI3ODcyMzQ5NzM2NSIsImV4cCI6MTcwODUxMjU5OCwiaWF0IjoxNzA4NTA1Mzk4LCJhaWQiOiJQdDZ2RU4zTFM2eTlyQWhEWHhTNnNnIiwiY2lkIjoiIn0.J6PzfK6SCJNnERbgv_GTvyId9ZjNCRva_FPLUOLVQio",
+              "join_url": "https://us04web.zoom.us/j/78723497365?pwd=67MuOC4RaEDM5wbu1NajMVnvadebNY.1",
+              "password": "6xTgtV",
+              "h323_password": "068699",
+              "pstn_password": "068699",
+              "encrypted_password": "67MuOC4RaEDM5wbu1NajMVnvadebNY.1"
+            }
+        """.trimIndent()
         val meeting = call {
-            meetings(
-                """
-                    {
-                      "uuid": "dzFrpLGqRcOtpiQQeAVDVA==",
-                      "id": 78723497365,
-                      "host_id": "lqkrEKqMR1CCmALIVs73RQ",
-                      "host_email": "zezulatomas@gmail.com",
-                      "topic": "Your Meeting Title",
-                      "type": 2,
-                      "status": "waiting",
-                      "start_time": "2024-02-21T08:49:58Z",
-                      "duration": 60,
-                      "timezone": "America/Los_Angeles",
-                      "created_at": "2024-02-21T08:49:58Z",
-                      "start_url": "https://us04web.zoom.us/s/78723497365?zak=eyJ0eXAiOiJKV1QiLCJzdiI6IjAwMDAwMSIsInptX3NrbSI6InptX28ybSIsImFsZyI6IkhTMjU2In0.eyJhdWQiOiJjbGllbnRzbSIsInVpZCI6Imxxa3JFS3FNUjFDQ21BTElWczczUlEiLCJpc3MiOiJ3ZWIiLCJzayI6IjAiLCJzdHkiOjEsIndjZCI6InVzMDQiLCJjbHQiOjAsIm1udW0iOiI3ODcyMzQ5NzM2NSIsImV4cCI6MTcwODUxMjU5OCwiaWF0IjoxNzA4NTA1Mzk4LCJhaWQiOiJQdDZ2RU4zTFM2eTlyQWhEWHhTNnNnIiwiY2lkIjoiIn0.J6PzfK6SCJNnERbgv_GTvyId9ZjNCRva_FPLUOLVQio",
-                      "join_url": "https://us04web.zoom.us/j/78723497365?pwd=67MuOC4RaEDM5wbu1NajMVnvadebNY.1",
-                      "password": "6xTgtV",
-                      "h323_password": "068699",
-                      "pstn_password": "068699",
-                      "encrypted_password": "67MuOC4RaEDM5wbu1NajMVnvadebNY.1"
-                    }
-                """.trimIndent()
-            ).create(
+            meetings(jsonResponse).create(
                 userId = USER_ID,
                 topic = "My Meeting",
                 startTime = LocalDateTime.now(CONSTANT_CLOCK),
@@ -48,7 +47,100 @@ class MeetingsTest : ModuleTest<Meetings>() {
                 timezone = CLIENT_TIMEZONE
             )
         }
-        assertNotNull(meeting, "Expected meeting to be created")
+        val expectedMeeting = parseJson(jsonResponse, MeetingResponse::class).toModel()
+        assertEquals(expectedMeeting, meeting, "Unexpected meeting")
+    }
+
+    @Test
+    fun `should get meeting`() = runBlocking {
+        val jsonResponse = """
+            {
+              "uuid": "dzFrpLGqRcOtpiQQeAVDVA==",
+              "id": 78723497365,
+              "host_id": "lqkrEKqMR1CCmALIVs73RQ",
+              "host_email": "zezulatomas@gmail.com",
+              "topic": "Your Meeting Title",
+              "type": 2,
+              "status": "waiting",
+              "start_time": "2024-02-21T08:49:58Z",
+              "duration": 60,
+              "timezone": "America/Los_Angeles",
+              "created_at": "2024-02-21T08:49:58Z",
+              "start_url": "https://us04web.zoom.us/s/78723497365?zak=eyJ0eXAiOiJKV1QiLCJzdiI6IjAwMDAwMSIsInptX3NrbSI6InptX28ybSIsImFsZyI6IkhTMjU2In0.eyJhdWQiOiJjbGllbnRzbSIsInVpZCI6Imxxa3JFS3FNUjFDQ21BTElWczczUlEiLCJpc3MiOiJ3ZWIiLCJzayI6IjAiLCJzdHkiOjEsIndjZCI6InVzMDQiLCJjbHQiOjAsIm1udW0iOiI3ODcyMzQ5NzM2NSIsImV4cCI6MTcwODUxMjU5OCwiaWF0IjoxNzA4NTA1Mzk4LCJhaWQiOiJQdDZ2RU4zTFM2eTlyQWhEWHhTNnNnIiwiY2lkIjoiIn0.J6PzfK6SCJNnERbgv_GTvyId9ZjNCRva_FPLUOLVQio",
+              "join_url": "https://us04web.zoom.us/j/78723497365?pwd=67MuOC4RaEDM5wbu1NajMVnvadebNY.1",
+              "password": "6xTgtV",
+              "h323_password": "068699",
+              "pstn_password": "068699",
+              "encrypted_password": "67MuOC4RaEDM5wbu1NajMVnvadebNY.1"
+            }
+        """.trimIndent()
+        val meeting = call { meetings(jsonResponse).get(78723497365) }
+        val expectedMeeting = parseJson(jsonResponse, MeetingResponse::class).toModel()
+        assertEquals(expectedMeeting, meeting, "Unexpected meeting")
+    }
+
+    @Test
+    fun `should delete meeting`() = runBlocking {
+        assertTrue(call { meetings().delete(78723497365) }, "Meeting not deleted")
+    }
+
+    @Test
+    fun `should delete all user meetings`() = runBlocking {
+        assertTrue(call { meetings().deleteAll(USER_ID) }, "User meetings not deleted")
+    }
+
+    @Test
+    fun `should list scheduled meetings`() = runBlocking {
+        val jsonResponse = """
+            {
+              "page_count": 2,
+              "page_number": 1,
+              "page_size": 30,
+              "total_records": 56,
+              "meetings": [
+                {
+                  "uuid": "7gH0gmRgSoeoo/1h0pHzAw==",
+                  "id": 76920264316,
+                  "host_id": "lqkrEKqMR1CCmALIVs73RQ",
+                  "topic": "Zoom Meeting",
+                  "type": 2,
+                  "start_time": "2024-02-21T08:47:59Z",
+                  "duration": 60,
+                  "timezone": "Europe/Prague",
+                  "agenda": "Test agenda",
+                  "created_at": "2024-02-21T08:47:59Z",
+                  "join_url": "https://us04web.zoom.us/j/76920264316?pwd=cUkK6ywWlauFusJori4chv9TZy5Ma5.1"
+                },
+                {
+                  "uuid": "dzFrpLGqRcOtpiQQeAVDVA==",
+                  "id": 78723497365,
+                  "host_id": "lqkrEKqMR1CCmALIVs73RQ",
+                  "topic": "Your Meeting Title",
+                  "type": 2,
+                  "start_time": "2024-02-21T08:49:58Z",
+                  "duration": 60,
+                  "timezone": "America/Los_Angeles",
+                  "created_at": "2024-02-21T08:49:58Z",
+                  "join_url": "https://us04web.zoom.us/j/78723497365?pwd=67MuOC4RaEDM5wbu1NajMVnvadebNY.1"
+                },
+                {
+                  "uuid": "wNYtPAewS3+RYENNrgvQ9g==",
+                  "id": 72781040142,
+                  "host_id": "lqkrEKqMR1CCmALIVs73RQ",
+                  "topic": "Test topic",
+                  "type": 2,
+                  "start_time": "2024-02-23T07:59:45Z",
+                  "duration": 60,
+                  "timezone": "Europe/London",
+                  "created_at": "2024-02-23T06:59:45Z",
+                  "join_url": "https://us04web.zoom.us/j/72781040142?pwd=95GC2CPDfDZiaMYDA1a31syE16N4ph.1"
+                }
+              ]
+            }
+        """.trimIndent()
+        val expectedPage = parseJson(jsonResponse, PaginationObject::class).toModel()
+        val page = call { meetings(jsonResponse).listScheduled(USER_ID) }
+        assertEquals(expectedPage, page, "Unexpected page")
     }
 
     private fun meetings(responseBody: String? = null): Meetings =
