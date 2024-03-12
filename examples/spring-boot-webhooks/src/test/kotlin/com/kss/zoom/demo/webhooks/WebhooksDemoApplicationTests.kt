a11aa7bd1e8084d4e577024283064cbad5bf5d35
zezutom <zezulatomas@gmail.com>
2024-03-12 22:21:00 +0100

    Fixed failing test.

--- examples/spring-boot-webhooks/src/test/kotlin/com/kss/zoom/demo/webhooks/WebhooksDemoApplicationTests.kt
+++ examples/spring-boot-webhooks/src/test/kotlin/com/kss/zoom/demo/webhooks/WebhooksDemoApplicationTests.kt
@@ -1,9 +1,10 @@
 package com.kss.zoom.demo.webhooks
 
+import com.kss.zoom.demo.webhooks.spring.WebhooksDemoApplication
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [WebhooksDemoApplication::class])
 class WebhooksDemoApplicationTests {
 
 	@Test
